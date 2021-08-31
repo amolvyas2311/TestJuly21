46edb466c4a10cda953654440e056af6cc47e9c5
Amol Vyas <amol.vyas2311.com>
2021-08-31 12:13:30 +0530

    Resolved_Conflicts

--- src/Test/Class_2.java
+++ src/Test/Class_2.java
@@ -3,10 +3,8 @@ package Test;
 public class Class_2 {
 	public static void main(String[] args) {
 		System.out.println("Branch D");
-		System.out.println("Branch_C");
+		System.out.println("Branch_CED");
 		System.out.println("Branch_C123");
-		System.out.println("Branch_C123");
-
 	}
 
 }
